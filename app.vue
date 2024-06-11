7a7b6853f6ca37717b6f523a390dab92caed9cbb
Jerry Lott <jj@Jerrys-Mac-mini.local>
2024-06-11 12:38:44 -0500

    Configured prettier

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="ma-5">
+    <v-card
+      title="Card title"
+      subtitle="subtitle"
+      text="..."
+      variant="outlined"
+    >
+      <v-card-actions>
+        <v-btn>Click me</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path"
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["vuetify-nuxt-module", "@pinia/nuxt"]
+  alias: {
+    "@": resolve(__dirname, "/"), 
+  },
+  modules: ["vuetify-nuxt-module"]
 })
